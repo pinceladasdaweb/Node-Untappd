1806c3d6ad16f40b7f3ddda5f49ff603c8787689
Pedro Rogério <pinceladasdaweb@hotmail.com>
2014-08-21 14:08:11 -0300

    Removed values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     untappd: {
-        client_id: '75DD96A9C8BE304BD6F1CE03DAFC01D9A1678A2B', // Your Client ID here
-        client_secret: '0CD4F873852CEBC66B4EC4C9BDE5C095F953E10C' // Your Client Secret here
+        client_id: '', // Your Client ID here
+        client_secret: '' // Your Client Secret here
     }
 };
